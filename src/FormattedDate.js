6fd7c695317b5aef7e2b3e88dfea0872e1ccc877
apilynb <102903798+apilynb@users.noreply.github.com>
2022-08-09 18:42:16 -0400

    Implemented Local time

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function FormattedDate(props) {
   let now = new Date(props.date);
+  let time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  console.log(time);
   let days = [
     "Sunday",
     "Monday",
@@ -12,17 +14,10 @@ export default function FormattedDate(props) {
     "Saturday",
   ];
   let weekday = days[now.getDay()];
-  let hour = now.getHours();
-  if (hour < 10) {
-    hour = `0${hour}`;
-  }
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+  
   return (
     <span>
-      <em>Last Updated:</em> <br /> {weekday} {hour}:{minutes}
+      <em>Last Updated:</em> <br /> {weekday} {time}
     </span>
   );
 }
